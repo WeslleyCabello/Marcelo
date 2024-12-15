ac453e9deb29a54bffc044a3aa42f08af1212185
WeslleyCabello <160623424+WeslleyCabello@users.noreply.github.com>
2024-12-15 17:47:51 -0300

    Initial commit

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rbPlayer;
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpForce = 15f;
+    [SerializeField] bool isJump;
+    [SerializeField] bool inFloor = true;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
+
+    Animator animPlayer;
+    private void Awake()
+    {
+        animPlayer = GetComponent<Animator>();
+        rbPlayer = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        inFloor = Physics2D.Linecast(transform.position, groundCheck.position, groundLayer);
+        Debug.DrawLine(transform.position, groundCheck.position, Color.blue);
+
+        animPlayer.SetBool("Jump", !inFloor);
+
+        if (Input.GetButtonDown("Jump") && inFloor)
+            isJump = true;
+        else if (Input.GetButtonUp("Jump") && rbPlayer.linearVelocity.y > 0)
+            rbPlayer.linearVelocity = new Vector2(rbPlayer.linearVelocity.x, rbPlayer.linearVelocity.y * 0.5f);
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        JumpPlayer();
+    }
+
+    void Move()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        rbPlayer.linearVelocity = new Vector2(xMove * speed, rbPlayer.linearVelocity.y);
+
+        animPlayer.SetFloat("speed", Mathf.Abs(xMove));
+
+        if (xMove > 0)
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        else if (xMove < 0)
+            transform.eulerAngles = new Vector3(0, 180, 0);
+    }
+
+    void JumpPlayer()
+    {
+        if (isJump)
+        {
+            rbPlayer.linearVelocity = Vector2.up * jumpForce;
+            isJump = false;
+        }
+    }
+}
